81bbe31262b1d50edc09e00706810428adf09ee2
acfan <acfan0523@gmail.com>
2023-07-31 23:30:23 +0800

    restyle

--- service/game-server-log-service/game-server-log-show-service/src/main/java/com/acvount/log/show/controller/LogController.java
+++ service/game-server-log-service/game-server-log-show-service/src/main/java/com/acvount/log/show/controller/LogController.java
@@ -2,15 +2,12 @@ package com.acvount.log.show.controller;
 
 import com.acvount.common.core.exception.BaseException;
 import com.acvount.common.core.result.Result;
-import com.acvount.log.show.pojo.LogVO;
 import com.acvount.log.show.service.LogService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author : acfan
