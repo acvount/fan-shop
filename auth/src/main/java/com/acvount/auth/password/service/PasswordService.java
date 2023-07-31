39293f893f65b119e7fa2e1010899c2c5d9e918d
acfan <acfan0523@gmail.com>
2023-07-31 23:30:02 +0800

    change UserService -> UserServiceApi

--- auth/src/main/java/com/acvount/auth/password/service/PasswordService.java
+++ auth/src/main/java/com/acvount/auth/password/service/PasswordService.java
@@ -2,7 +2,7 @@ package com.acvount.auth.password.service;
 
 import com.acvount.auth.login.service.password.strategy.PasswordEncryptionStrategy;
 import com.acvount.common.core.exception.BaseException;
-import com.acvount.user.api.UserService;
+import com.acvount.user.api.UserServiceApi;
 import com.acvount.user.bean.UserAuthorization;
 import com.acvount.web.LoginUser;
 import org.apache.dubbo.config.annotation.DubboReference;
@@ -19,16 +19,16 @@ public class PasswordService {
 
     @DubboReference
     @SuppressWarnings("unused")
-    private UserService userService;
+    private UserServiceApi userServiceApi;
 
     public Boolean changePassword(String newPassword) throws BaseException {
-        UserAuthorization userAuthorization = userService.getUserAuthorizationById(LoginUser.getLoginUserID());
+        UserAuthorization userAuthorization = userServiceApi.getUserAuthorizationById(LoginUser.getLoginUserID());
         PasswordEncryptionStrategy strategy = PasswordEncryptionStrategy.getStrategy();
         String[] result = strategy.encryptPassword(newPassword);
         userAuthorization.setSalt(result[0]);
         userAuthorization.setPasswordHash(result[1]);
         userAuthorization.setEncryptionType(strategy.getEncryptionType());
-        Integer i = userService.updateUserAuthorizationById(userAuthorization);
+        Integer i = userServiceApi.updateUserAuthorizationById(userAuthorization);
         return i > 0;
     }
 
