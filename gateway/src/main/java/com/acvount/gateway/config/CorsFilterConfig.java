4bc2829ddf8b42e81fe3dbfbcc3356acb7524795
acfan <acfan0523@gmail.com>
2023-08-04 15:22:12 +0800

    cors config

---
+++ gateway/src/main/java/com/acvount/gateway/config/CorsFilterConfig.java
@@ -0,0 +1,32 @@
+package com.acvount.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+
+/**
+ * @author : acfan
+ * date : create in 2023/8/1 20:26
+ * description :
+ **/
+
+@Configuration
+public class CorsFilterConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList("GET,POST,PUT,DELETE,OPTIONS".split(",")));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
