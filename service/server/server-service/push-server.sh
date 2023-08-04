e5b26c6ecc3c09c7f1363602ff596b8d4851c694
acfan <acfan0523@gmail.com>
2023-08-04 15:22:01 +0800

    server online

---
+++ service/server/server-service/push-server.sh
@@ -0,0 +1,2 @@
+docker login --username=t-3369458326-1 --password cc1234567. registry.cn-beijing.aliyuncs.com
+docker buildx build -t registry.cn-beijing.aliyuncs.com/acvount/server-service:1.0 --platform=linux/amd64 . --push
