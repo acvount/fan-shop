dab294203a2b81792e7121ce6c6eabc2ba75e264
acfan <acfan0523@gmail.com>
2023-08-08 11:40:42 +0800

    add catch exception

--- service/game-server-log-service/game-server-log-handle-service/src/main/java/com/acvount/server/log/handle/LogListener.java
+++ service/game-server-log-service/game-server-log-handle-service/src/main/java/com/acvount/server/log/handle/LogListener.java
@@ -3,13 +3,13 @@ package com.acvount.server.log.handle;
 import com.acvount.server.log.dto.LogMessage;
 import com.acvount.server.log.handle.stage.LogStage;
 import com.acvount.server.log.handle.stage.LogTypeStageChecker;
+import com.acvount.server.log.lose.service.LoseLogService;
 import com.alibaba.fastjson.JSONObject;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.support.ErrorMessage;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -28,25 +28,24 @@ public class LogListener {
 
     @Resource
     private LogTypeStageChecker logTypeStageChecker;
+    @Resource
+    private LoseLogService loseLogService;
 
     @Bean
     public Function<Flux<Message<String>>, Mono<Void>> logMessageConsumer() {
         return flux -> flux.map(message -> {
-            LogMessage logMessage = JSONObject.parseObject(message.getPayload(), LogMessage.class);
-            LogStage stage = logTypeStageChecker.getStage(logMessage.getType());
-            if (Objects.isNull(stage)) {
-                log.error("no stage : {}", logMessage);
-                return message;
+            try {
+                LogMessage logMessage = JSONObject.parseObject(message.getPayload(), LogMessage.class);
+                LogStage stage = logTypeStageChecker.getStage(logMessage.getType());
+                if (Objects.isNull(stage)) {
+                    log.error("no stage : {}", logMessage);
+                    return message;
+                }
+                stage.consumer(logMessage);
+            } catch (Exception e) {
+                loseLogService.addLoseLog(111L, message.getPayload(), "handle-message:" + e.getMessage());
             }
-            stage.consumer(logMessage);
             return message;
         }).then();
     }
-
-    @SuppressWarnings("unused")
-    public void error(Message<?> message) {
-        ErrorMessage errorMessage = (ErrorMessage) message;
-        log.error("Handling ERROR, errorMessage = {} ", errorMessage);
-    }
-
 }
